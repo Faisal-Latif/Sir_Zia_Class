09553edb8db9505c69bd30e0ddfbc5936aa42f05
Faisal Latif <faisal.latif1295@gmail.com>
2023-08-27 09:12:46 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(2, 3));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function sum (num1:number,num2:number){
+    return num1 + num2
+}
+
+console.log(sum(2,3));
